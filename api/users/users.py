52e5381f261bea72e9dea2ceedc881128ffaa1da
codygreen <cody@codygreen.com>
2023-04-02 13:10:09 -0500

    add and list users

---
+++ api/users/users.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
+from pymongo import MongoClient
+import uuid
+import sys
+
+sys.path.append("..")
+
+import settings
+
+class User(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    name: str
+    password: str
+    type: str
+    familyId: str
+
+app = FastAPI()
+client = MongoClient(settings.MONGO_URI)
+db = client.allowance
+
+@app.get("/", response_model=list[User])
+async def show_list():
+    collection = db.users
+    cursor = collection.find({})
+    users = []
+    for document in cursor:
+        users.append(document)
+    return users
+
+@app.post("/", status_code=status.HTTP_201_CREATED, response_model=User)
+async def create_user(user: User):
+    collection = db.users
+    user = jsonable_encoder(user)
+    new_user = collection.insert_one(user)
+    created_user = collection.find_one({"_id": new_user.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
