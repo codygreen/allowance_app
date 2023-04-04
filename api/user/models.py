52668fd126560caaa243b95bdf3288f78822c8b0
codygreen <cody@codygreen.com>
2023-04-04 13:48:48 -0500

    add balance to model

--- api/user/models.py
+++ api/user/models.py
@@ -6,16 +6,16 @@ from typing import Literal
 
 class User(Document):
     name: str
-    password: str
     type: Literal["guardian", "child", "contributor"]
     familyId: PydanticObjectId
+    balance: float = Field(default=0)
 
     class Config:
         schema_extra = {
             "example": {
                 "name": "John",
-                "password": "1234",
                 "type": "guardian",
-                "familyId": "642b09500fec1b9fe4ab78ae"
+                "familyId": "642b09500fec1b9fe4ab78ae",
+                "balance": 0
             }
         }
