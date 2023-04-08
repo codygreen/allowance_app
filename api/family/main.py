af4f80d33778929888b5b9084f6697789dcd8961
codygreen <cody@codygreen.com>
2023-04-08 06:42:21 -0500

    add shutdown event

--- api/family/main.py
+++ api/family/main.py
@@ -19,5 +19,11 @@ class Settings(BaseSettings):
 async def app_init():
     """Initialize the app"""
     client = motor.motor_asyncio.AsyncIOMotorClient(Settings().mongodb_url)
+    app.mongodb_client = client
     await init_beanie(client.get_default_database(), document_models=[Family])
     app.include_router(family_router, prefix="/v1/family")
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    """Close the database connection"""
+    app.mongodb_client.close()
